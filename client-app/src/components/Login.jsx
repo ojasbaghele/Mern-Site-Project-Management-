5951b592707f43ebf40673c76109e025f2df6acd
Ojas Baghele <ojas.baghele@gmail.com>
2024-02-13 13:53:09 +0530

    Register State Added ui Changes on state Change

--- client-app/src/components/Login.jsx
+++ client-app/src/components/Login.jsx
@@ -2,18 +2,32 @@ import React, { useState } from 'react';
 import '../assets/Login.css';
 import bg1 from '../assets/images/login-bg-1.svg';
 import logo from '../assets//images/Logo.svg';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+// const navigate = useNavigate();
 
 export default function Login() {
+  const [name, set_name] = useState("");
   const [email, set_email] = useState("");
   const [password, set_password] = useState("");
-  // const Login_user = useState({
-  //   email: email,
-  //   password: password,
-  // });
-  const navigate = useNavigate();
+  const [pageName, setPageName] = useState("Log In");
 
-  //! ---------------------- Handle OnChange -----------------------
+
+  //! ------------------------ Page State  --------------------------- 
+
+  function loginBtn() {
+    setPageName("Log In");
+    document.getElementById('UserName').classList.add("hidden");
+  }
+  function signupBtn() {
+    setPageName("Sign Up");
+    document.getElementById('UserName').classList.remove("hidden");
+  }
+
+  //! ----------------------------------------------------------------
+  //! ----------------------- Handle OnChange ------------------------
+  const handleName = (event) => {
+    set_name(event.target.value);
+  }
   const handleEmail = (event) => {
     set_email(event.target.value);
     if (email.length >= 1 && email.length <= 10) {
@@ -36,40 +50,65 @@ export default function Login() {
   }
   //! ----------------------------------------------------------------
   //! ------------------- OnSubmit - Login Function ------------------
+
   async function LogIn(event) {
     event.preventDefault();
-    let Login_user = {
-      "email": email,
-      "password": password
-    }
-    console.log(Login_user);
-    try {
-      const response = await fetch('http://localhost:3030/login', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(Login_user),
-      });
-      console.log(response);
-      if (response.ok) {
-        alert("User Logged in successfully")
-      } else {
-        alert("response is Not 'OK' ");
+    if (page == "Log In") {
+      let Login_user = {
+        "email": email,
+        "password": password
       }
-    } catch (error) {
-      console.log("User Data Not Sent", error);
+      console.log(Login_user);
+      try {
+        const response = await fetch('http://localhost:3030/login', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(Login_user),
+        });
+        console.log(response);
+        if (response.ok) {
+          alert("User Logged in successfully")
+        } else {
+          alert("response is Not 'OK' ");
+        }
+      } catch (error) {
+        console.log("User Data Not Sent", error);
+      }
+    } else {
+      let Signup_user = {
+        "name": name,
+        "email": email,
+        "password": password
+      }
+      console.log(Signup_user);
+      try {
+        const response = await fetch('http://localhost:3030/login', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(Signup_user),
+        });
+        console.log(response);
+        if (response.ok) {
+          alert("User Registered in successfully")
+        } else {
+          alert("response is Not 'OK' ");
+        }
+      } catch (error) {
+        console.log("User Data Not Sent", error);
+      }
     }
-
-    // if(password.length <= 6){
-    //   document.getElementById('pass_warning').innerHTML = "<p>* Password Cannot Be lessthan 6 characters</p>";
-    //   document.getElementById('pass_warning').classList.remove("hidden");
-    // }
-    // if(email.length <= 12){
-    //   document.getElementById('email_warning').classList.remove("hidden");
-    //   document.getElementById('email_warning').innerHTML = "<p>* Invalid Email</p>";
-    // }else{
-    //   navigate("/ProjectManagement/Dashboard");
-    // }
   }
+
+  // if(password.length <= 6){
+  //   document.getElementById('pass_warning').innerHTML = "<p>* Password Cannot Be lessthan 6 characters</p>";
+  //   document.getElementById('pass_warning').classList.remove("hidden");
+  // }
+  // if(email.length <= 12){
+  //   document.getElementById('email_warning').classList.remove("hidden");
+  //   document.getElementById('email_warning').innerHTML = "<p>* Invalid Email</p>";
+  // }else{
+  //   navigate("/ProjectManagement/Dashboard");
+  // }
   //! ----------------------------------------------------------------
 
   return (
@@ -79,8 +118,16 @@ export default function Login() {
         <img id='logo' src={logo} alt='./logos/Logo-m.svg' />
         <h3 id='color-w'>Online Project Management</h3>
 
+        <div className='pageSwitch'>
+          <button onClick={loginBtn}>Log In</button>
+          <button onClick={signupBtn}>Sign up</button>
+        </div>
         <form className='login-form' onSubmit={LogIn}>
-          <h1 id="formHeading">Login to get started</h1>
+          <h1 id="formHeading"> {pageName} </h1>
+          <div className='input-field' id="UserName">
+            <label>Name</label>
+            <input required id='Uname' onChange={handleName} name='name' type='text'></input >
+          </div>
           <div className='input-field'>
             <label>Email</label>
             <input required id='Uemail' onChange={handleEmail} name='email' type='email'></input >
@@ -92,7 +139,7 @@ export default function Login() {
             <spam id='pass_warning' className='warning_txt hidden'><p>* Invalid Password</p></spam>
             <span id='f-password'><p>Forgot Password?</p></span>
           </div>
-          <button id='btnLOGIN' type='submit' >Login</button>
+          <button id='btnLOGIN' type='submit'> {pageName} </button>
         </form>
       </div>
     </>
